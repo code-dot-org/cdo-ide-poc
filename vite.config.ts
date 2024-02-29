0a0b640679ea5d2e34ed69f736b47fd50aa28bb2
Jim Thomason <jim@japanesewartuba.com>
2024-02-29 12:03:55 -0600

    jimt/CDOIDE - lets see if rollup works right

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,36 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/cdo-codemirror-editor-poc",
   plugins: [react()],
+  build: {
+    sourcemap: true,
+    lib: {
+      entry: {
+        "cdo-ide": path.resolve(__dirname, "src/exports/index.ts"),
+      },
+      formats: ["es"],
+    },
+
+    rollupOptions: {
+      external: [
+        "react",
+        "react-dom",
+        "@codemirror/lang-css",
+        "@codemirror/lang-html",
+        "@codemirror/lang-javascript",
+        "@codemirror/lang-json",
+      ],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+        preserveModules: false,
+      },
+    },
+  },
 });
